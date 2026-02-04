31546c05ccfb59003c105d86f40ccbdd9ca7affa
smartwachira <150593527+smartwachira@users.noreply.github.com>
2026-02-04 18:07:55 +0300

    Refactor database configuration; use DATABASE_URL for Sequelize connection and streamline SSL options

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
+//import '/index.css'
+import axios from 'axios';
+
+//Set the Base URL dynamically
+//If we are in production (Vercel), use the Environment Variable
+//If we are in development (Localhost), use nothing (relies on proxy)
+if (import.meta.env.VITE_API_URL){
+  axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- server/config/db.js
+++ server/config/db.js
@@ -5,15 +5,20 @@ dotenv.config();
 
 //Create the sequelize instance
 const sequelize = new Sequelize(
-    //The security Layer (Environment Variables)
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    //The security Layer (Environment Variables): "All-in -one Connection String"
+    process.env.DATABASE_URL,
     {
         //The Location & Language
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_Dialect,
-        logging:false, //Disable logging every SQL query to the console (cleaner)
+        dialect: "postgres",
+        //The "Smart Switch"(Secure Sockets Layer(SSL) Configuration)
+        dialectOptions: {
+            ssl: process.env.NODE_ENV === 'production' ? {
+                require: true,
+                rejectUnauthorized: false
+            } : false
+        },
+        logging:false,
+         //Disable logging every SQL query to the console (cleaner)
     }
 );
 
